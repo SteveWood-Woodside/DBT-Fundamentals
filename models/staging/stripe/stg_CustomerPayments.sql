4a7f7814e7a775321e680f59d44fb350d8ef7c51
Steve Wood <Steve.Wood@woodside.com.au>
2022-04-25 07:40:01 +0000

    payments

---
+++ models/staging/stripe/stg_CustomerPayments.sql
@@ -0,0 +1,16 @@
+
+
+    SELECT
+        O.CUSTOMER_ID,
+        sum(P.ORDERAMOUNT) as totalAmountperCustomer
+        
+    FROM
+         {{ ref('fct_orders') }} AS O
+    LEFT OUTER JOIN
+         {{ ref('stg_payments') }} AS P
+        ON
+            O.ORDER_ID = P.ORDERID
+    WHERE
+        P.PAYMENTSTATUS = 'success'
+    GROUP BY
+        O.CUSTOMER_ID        
